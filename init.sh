d360ab529833a6bc41dbb07443676318efa699df
Bret Mogilefsky <bmogilefsky@gmail.com>
2016-10-20 13:36:42 -0700

    Cloud Foundry forward-compatibility
    Per [Cloud Foundry documentation](https://docs.cloudfoundry.org/running/apps-enable-diego.html#app-code) use of `$VCAP_APP_HOST` and `$VCAP_APP_PASSWORD` are incompatible with newer (Diego-based) Cloud Foundry deployments. This change makes this repository forward compatible with the new cloud.gov environment deployment in AWS GovCloud.

--- init.sh
+++ init.sh
@@ -9,4 +9,4 @@ echo "------- load data ------"
 python abb/manage.py loaddata hours/fixtures/initial_data.json
 
 echo "------ starting django &nbsp;------"
-cd abb && waitress-serve --port=$VCAP_APP_PORT abb.wsgi:application
+cd abb && waitress-serve --port=$PORT abb.wsgi:application
